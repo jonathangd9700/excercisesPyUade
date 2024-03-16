119d8fc9346b1350761bd4c2314c2ad55d211478
jonathangd9700 <jonathan-g-dominguez@outlook.es>
2024-03-16 11:02:30 -0300

    Ejercicio 1

--- Unidad1.py
+++ Unidad1.py
@@ -1,4 +1,32 @@
-def cargarNum():
-    print("Hola")
+def  cargarNum():
+    lista = []
+    i = 0
+    while (i<3):
+        print("Ingrese un numero")
+        numero = int(input())
+        lista.append(numero)
+        i+=1
+    return lista
 
-cargarNum()
+def buscarMayor(lista):
+    mayor = lista[0]
+    encontrado = 0
+    estricto = False
+    for i in range(len(lista)):
+        if(lista[i]>mayor):
+            mayor= lista[i]
+    for i in range(len(lista)):
+        if(mayor == lista[i]):
+            encontrado+=1
+    if(encontrado >1):
+        estricto = True
+    if (estricto == False):
+        return -1
+    else:
+        return mayor
+    
+
+result = cargarNum()
+print(result)
+
+buscarMayor(result)
