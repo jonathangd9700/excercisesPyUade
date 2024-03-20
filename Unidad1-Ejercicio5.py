a069dfd7f78d499d35a716c293c93e1e05d38ee1
jonathangd9700 <jonathan-g-dominguez@outlook.es>
2024-03-20 13:16:22 -0300

    Ejercicio 5b finalizado no tal cual a la consigna

--- Unidad1-Ejercicio5.py
+++ Unidad1-Ejercicio5.py
@@ -13,3 +13,31 @@ if(verificar == True):
     print("El numero es oblongo")
 else:
     print("El numero no es oblongo")
+    
+"""5 - B ---------------------------"""
+
+def triangular(numero,combinaciones):
+    esTriangular = False
+    for i in range(len(combinaciones)):
+        if numero == combinaciones[i]:
+            esTriangular = True
+    if esTriangular == True:
+        print("Es triangular")
+    else:
+        print("No es triangular")
+
+combinaciones = []
+acumulador = 0
+lista = [1,2,3,4,5,6,7,8,9]
+vueltas = 0
+for i in range(len(lista)):
+    if vueltas == 0:
+        acumulador = 1
+    else:
+        acumulador = 0
+    for a in range(i+1,len(lista)):
+        acumulador +=  lista[a]
+        combinaciones.append(acumulador)
+    vueltas = 1
+numero = int(input("Ingrese un numero para saber si es triangular"))
+triangular(numero,combinaciones)
