b72c702f76358b4b4fd5552a2abc0cbb65110e7a
Jonathan Dominguez <jonathan-g-dominguez@outlook.es>
2024-04-07 12:46:56 -0300

    Ejercicio 6 finalizado

--- Unidad4/Ejercicio6.py
+++ Unidad4/Ejercicio6.py
@@ -1,13 +1,42 @@
 cadena = "El número de teléfono es 4356-7890"
 
+"""
+#Con slice()
+
 def cortarCadena(pos,caract,cadena):
     i=0
     frase = []
     while i <= caract:
         recorte = slice(pos,pos+i)
+        print(recorte)
         i+=1
     return cadena[recorte]
 
 recortado = cortarCadena(25,9,cadena)
 
 print(recortado)
+"""
+
+"""
+def cortarCadena(inicio,caract,cadena):
+    final = inicio + caract
+    fraseFinal = cadena[inicio:final]
+    return fraseFinal
+"""
+
+#Sin slice()
+
+def cortarCadena(inicio,caract,cadena):
+
+    frase = []
+    fraseFinal = ""
+    fin = (inicio + caract)-1
+    while inicio <= fin:
+        frase.append(cadena[inicio])
+        inicio+=1
+    for i in range(len(frase)):
+        fraseFinal += frase[i]
+    return fraseFinal
+
+frase = cortarCadena(25,9,cadena)
+print(frase)
